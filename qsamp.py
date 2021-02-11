7f2e8e7f4f0bafa0c6a30e45a844e32d6aca2ab1
Francesco Rizzi <fnrizzi@sandia.gov>
2021-02-11 17:20:01 +0100

    update

--- qsamp.py
+++ qsamp.py
@@ -32,7 +32,6 @@ def doPlot(ax, th, r, z, bd):
 
   ax.set_ylim([cmbRadius, earthRadius])
   ax.set_yticks([]) #[3480, 5701, 6371])
-  #plt.yticks(fontsize=13)
   ax.set_thetamin(-90)
   ax.set_thetamax(90)
   ax.set_xticks(np.pi/180. * np.linspace(-90, 90., 7, endpoint=True))
@@ -59,25 +58,17 @@ if __name__== "__main__":
   th, r = th0.reshape((nr,nth)), r0.reshape((nr,nth))
 
   d = np.loadtxt("./snaps_vp_0", skiprows=1)
-  # print(np.min(d0), np.max(d0))
-  # d = 2.*(d0 - np.min(d0))/np.ptp(d0)-1.
-  # print(np.min(d), np.max(d))
-  # print(d.shape)
 
   U, lam, VH = np.linalg.svd(d, full_matrices=False)
 
-  s = 100
+  s = 20
 
   fig1 = plt.figure(0)
   ax = fig1.add_subplot(111, projection='polar')
   doPlot(ax, th, r, d[:,-1].reshape((nr, nth)), [-5e-9, 5e-9])
 
-  q4, r4, p4 = linalg.qr(U.T, pivoting=True)
-  ax.scatter(th0[p4[0:s]], r0[p4[0:s]], c='b', s=10, zorder=2)
+  q, r, p = linalg.qr(U[:,:s].T, pivoting=True)
+  ax.scatter(th0[p[0:s]], r0[p[0:s]], c='b', s=10, zorder=2)
 
   plt.tight_layout()
   plt.show()
-
-
-
-  #doPlot1(th, r, d[:,-1].reshape((nr, nth)), 1,[-5e-9, 5e-9])
